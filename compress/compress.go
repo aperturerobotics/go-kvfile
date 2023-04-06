7e39d40d0af8c960fe287dd117f286d942e20258
Christian Stewart <christian@paral.in>
2023-04-06 02:07:35 -0700

    feat: add seekable zstd compression
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ compress/compress.go
@@ -0,0 +1,69 @@
+package kvfile_compress
+
+import (
+	"io"
+
+	seekable "github.com/SaveTheRbtz/zstd-seekable-format-go"
+	"github.com/klauspost/compress/zstd"
+	kvfile "github.com/paralin/go-kvfile"
+)
+
+// WriteCompress writes the given key/value pairs to the store in writer.
+// Uses seekable zstd compression.
+//
+// Serializes and writes the key/value pairs.
+// Note: keys will be sorted by key.
+// Note: keys must not contain duplicate keys.
+// writeValue should write the given value to the writer returning the number of bytes written.
+func WriteCompress(writer io.Writer, keys [][]byte, writeValue func(wr io.Writer, key []byte) (uint64, error)) error {
+	zenc, err := zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedBestCompression))
+	if err != nil {
+		return err
+	}
+	defer zenc.Close()
+
+	w, err := seekable.NewWriter(writer, zenc)
+	if err != nil {
+		return err
+	}
+
+	if err := kvfile.Write(w, keys, writeValue); err != nil {
+		return err
+	}
+
+	// Close and flush seek table.
+	return w.Close()
+}
+
+// ReadSeekerAt is the interface BuildCompressReader accepts.
+type ReadSeekerAt interface {
+	io.ReadSeeker
+	io.ReaderAt
+}
+
+// BuildCompressReader reads key/value pairs from the compressed reader.
+// Uses seekable zstd compression.
+func BuildCompressReader(rd ReadSeekerAt) (*kvfile.Reader, error) {
+	dec, err := zstd.NewReader(nil)
+	if err != nil {
+		return nil, err
+	}
+	r, err := seekable.NewReader(rd, dec)
+	if err != nil {
+		dec.Close()
+		return nil, err
+	}
+	size, err := r.Seek(0, io.SeekEnd)
+	if err != nil {
+		dec.Close()
+		_ = r.Close()
+		return nil, err
+	}
+	kvReader, err := kvfile.BuildReader(r, uint64(size))
+	if err != nil {
+		dec.Close()
+		_ = r.Close()
+		return nil, err
+	}
+	return kvReader, nil
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,17 @@ module github.com/paralin/go-kvfile
 go 1.20
 
 require (
+	github.com/SaveTheRbtz/zstd-seekable-format-go v0.6.1
+	github.com/klauspost/compress v1.16.4
 	github.com/pkg/errors v0.9.1
 	google.golang.org/protobuf v1.30.0
 )
 
-require github.com/google/go-cmp v0.5.8 // indirect
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/google/btree v1.1.2 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+)
